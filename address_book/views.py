a257e29682db9a81f865c5b5e2074a3cd1e7c006
Utkarsh srivastava <utak6543@gmail.com>
2020-01-31 23:47:54 +0530

    done al thing

--- address_book/views.py
+++ address_book/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Address
+from .forms import AddressForm
+from django.contrib import messages
 def home(request):
-    return render(request,'home.html',{})
+    all_address = Address.objects.all
+    return render(request,'home.html',{'all_address':all_address})
 def add_address(request):
-    return render(request,'add_address.html',{})
+    if request.method == 'POST':
+        form = AddressForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request,('Address has been added!!'))
+            return redirect('home')
+        else :
+            messages.success(request,'seems like there was an error!')
+            return render(request,'add_address.html',{})
+    else:
+        return render(request, 'add_address.html', {})
+def edit(request, list_id):
+    if request.method == 'POST':
+        current_address = Address.objects.get(pk=list_id)
+        form = AddressForm(request.POST or None, instance = current_address)
+        if form.is_valid():
+            form.save()
+            messages.success(request,('Address has been edited!!'))
+            return redirect('home')
+        else :
+            messages.success(request,'seems like there was an error!')
+            return render(request,'edit.html',{})
+    else:
+        get_address = Address.objects.get(pk=list_id)
+        return render(request, 'edit.html', {'get_address': get_address})
+def delete(request, list_id):
+    if request.method == 'POST':
+        current_address = Address.objects.get(pk=list_id)
+        current_address.delete()
+        messages.success(request, ('Address has been deleted!!'))
+        return redirect('home')
+    else:
+        messages.success(request, ('Nothing to see here!!'))
+        return redirect('home')
